499fc2444ac7a8aa360dad28320775c181fad2af
SimoneScapin <simonescap87@gmail.com>
2022-04-06 16:26:40 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,50 +1,49 @@
-/*
-Descrizione:
+// faccio visualizzare in pagina 5 numeri casuali
+const myUl = document.getElementById("genera");
+const arrayRandomN = [];
+const arrayIns = [];
 
-Visualizzare in pagina 5 numeri casuali.
-Da lì parte un timer di 30 secondi.
-Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. 
-*/
-
-
-
-//Creo una variabile che mi generi 5 numeri random da 1 a 100 che implementerò poi nella timing function per 30sec
-//Creo una costante dove metterò i numeri scelti con un push e li paragonerò con i numeri generati per vedere quanti corrispondono 
-let randomNumbers = [];
-let fixedValue=5;
-let chosenNumbers = [];
-let time = 0;
-
-
-//Funzione che mi crea 5 numeri random (devo aggiungere una condizione che verifichi che non siano doppi)
-document.getElementById('start').addEventListener('click', function(){   
-    for ( let i = 0; i < 5; i++){
-        randomNumbers =  Math.floor((Math.random() * 100) + 1);
-        document.getElementById('text').innerHTML += randomNumbers;
-        console.log(randomNumbers); 
-        setTimeout(function(){
-            document.getElementById("text").innerHTML = '';
-        }, 3000);
-    }
-    
-    chosenNumbers = setInterval( function(){
-        for (let i = 0; i < 5; i++){
-            time++;
-            chosenNumbers = parseInt(prompt('flkjndskf'));
-        }
-        console.log(time)
-    },4000)
-   
-})
-
-
-
-
+for (i = 1; i <= 5; i++) {
+    const randomNumber = Math.floor(Math.random() * 100);
+    const myElement = `<li> ${randomNumber}</li>`;
+    myUl.innerHTML += myElement;
+    arrayRandomN.push(randomNumber);
+}
+console.log(arrayRandomN);
 
 /**
-    Devo creare una funzione che chieda all'utente 5 numeri  quali fossero i numeri apparsi in precedenza tramite un prompt
-    usando un ciclo while? i < 5 i++ 
-    ,
-
+ *  Da lì parte un timer di 30 secondi.
+    Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+    Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
  */
+
+
+setTimeout(funzioneScomparsaNumeri, 5000);
+function funzioneScomparsaNumeri() {
+    myUl.classList.add('display-none');
+    setTimeout(miaFunzione, 250);
+}
+
+function miaFunzione() {
+    for (i = 0; i < 5; i++) {
+        let inserisciNumero = parseInt(prompt("Inserisci i numeri che hai visto, uno alla volta, nella pagina"));
+        arrayIns.push(inserisciNumero);
+    }
+
+let ok = 0;
+let ko = 0;
+
+    // qui devo fare il controllo
+    for (let i = 0; i < arrayIns.length; i++) {
+        const element = arrayIns[i];
+        if (arrayRandomN.includes(element)){
+            ok++
+        } else {
+            ko++
+        }
+    }
+    document.getElementById('testo').innerHTML = `Hai indovinato ${ok} numero/i, non hai indovinato ${ko} numeri/o`;
+
+}
+
+console.log(arrayIns)
